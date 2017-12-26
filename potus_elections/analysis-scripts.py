ba8afef3d3e754b4c825d83efbcf2bbf154738d4
Sreejith S <sreejith2904@gmail.com>
2017-12-26 01:08:16 -0500

    update with simple binary classification neural net

---
+++ potus_elections/analysis-scripts.py
@@ -0,0 +1,117 @@
+## imports 
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+
+__author__ = "Sreejith Sreekumar"
+__email__ = "sreekumar.s@husky.neu.edu"
+__version__ = "0.0.1"
+
+data_folder = "data/"
+train_file = data_folder + "train.csv"
+
+
+test_file = data_folder + "test.csv"
+
+data = pd.read_csv(train_file)
+
+msk = np.random.rand(len(data)) < 0.8
+
+train = data[msk]
+test = data[~msk]
+
+one_hot_of_winner = pd.get_dummies(train['Winner'])
+one_hot_of_winner_test = pd.get_dummies(test['Winner'])
+
+train.drop(['Winner'], axis=1, inplace=True)
+test.drop(['Winner'], axis=1, inplace=True)
+X_train = train.as_matrix()
+Y_train = one_hot_of_winner.as_matrix()
+
+X_test = test.as_matrix()
+Y_test = one_hot_of_winner_test.as_matrix()
+
+
+learning_rate = 0.001
+training_epochs = 100
+batch_size = 100
+display_step = 1
+
+n_hidden_1 = 20 # 1st layer number of features
+n_hidden_2 = 20 # 2nd layer number of features
+n_input = 14 # Number of feature
+n_classes = 2 # Number of classes to predict
+
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+
+
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(len(X_train)/batch_size)
+        X_batches = np.array_split(X_train, total_batch)
+        Y_batches = np.array_split(Y_train, total_batch)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_x, batch_y = X_batches[i], Y_batches[i]
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_x,
+                                                          y: batch_y})
+            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost))
+    print("Optimization Finished!")
+
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    # Calculate accuracy
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print("Accuracy:", accuracy.eval({x: X_test, y: Y_test}))
+    global result 
+    result = tf.argmax(pred, 1).eval({x: X_test, y: Y_test})
